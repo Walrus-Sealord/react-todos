f6da55b4131eed8341cbd8750bf9e4a3252035d7
WalrusSealord <mbradleymark@gmail.com>
2016-09-23 08:24:09 +0100

    Added new task form. Not working. Time for breakfast

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,25 @@
 import React, {Component, PropTypes} from "react";
+import ReactDOM from "react-dom";
 import {createContainer} from "meteor/react-meteor-data";
 import {Tasks} from "../api/tasks.js";
 import Task from "./Task.jsx";
 
 // App component - represents the whole app
 class App extends Component {
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log("Event fired");
+        // Find field with the React ref
+        const text = ReactDom.findDOMNode(this.refs.textInput).value.trim();
+
+        Tasks.insert({
+            'text': "dummy",
+            createdAt: new Date(),
+        });
+
+        // cLear Form
+        ReactDOM.findDOMNode(this.refs.textInput).value = "";
+    }
     renderTasks() {
         return this.props.tasks.map((task) => (
             <Task key={task._id} task={task}/>
@@ -16,6 +31,15 @@ class App extends Component {
             <div className="container">
                 <header>
                     <h1>Task List</h1>
+                    {/* Form for adding new Tasks */}
+                    < form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+                        <input
+                            type="text"
+                            ref="textInput"
+                            placeholder="Add a new task"
+                        />
+                    </form>
+
                 </header>
 
                 <ul>
